c597a69bc8934c965f709e40440befa490bc5755
Evgeny Artemov <eartemov013@gmail.com>
2022-11-19 11:27:52 +0400

    add sign in methods

--- auth.ts
+++ auth.ts
@@ -1,5 +1,40 @@
+import { Request, Response, NextFunction } from 'express';
 import jwt, { Secret } from 'jsonwebtoken';
+// @ts-ignore
+import bcrypt from 'bcrypt';
 
 // secret available because of dotenv in index.js
 const secret = process.env.JWT_SECRET as Secret;
-export const createJWT = ({ id, username }: {id: string, username: string }) => jwt.sign({ id, username }, secret)
+
+// for sign in
+export const comparePasswords = (pass: string, hash: string): Promise<boolean> => bcrypt.compare(pass, hash);
+
+export const hashPassword = (pass: string) => bcrypt.hash(pass, 42);
+export const createJWT = ({ id, username }: {id: string, username: string }) => jwt.sign({ id, username }, secret);
+
+export const protect = (req: Request&{user?: any}, res: Response, next: NextFunction) => {
+  const bearer = req.headers.authorization;
+  if(!bearer) {
+    res.status(401);
+    res.json({ message: 'Not Authorized!' });
+    return;
+  }
+
+  const [, token] = bearer.split(' ');
+
+  if(!token) {
+    res.status(401);
+    res.json({ message: 'Not valid token!' });
+    return;
+  }
+
+  try {
+    const user = jwt.verify(token, secret);
+    req.user = user;
+    next();
+  } catch (e) {
+    console.error(e);
+    res.status(401);
+    res.json({ message: 'Not valid token!' });
+  }
+}
