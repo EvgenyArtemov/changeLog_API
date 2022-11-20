91eea6b6f970d31d658e4ba2e6c936c5d30070ed
Evgeny Artemov <eartemov013@gmail.com>
2022-11-20 20:41:50 +0400

    mb fix

---
+++ db.ts
@@ -0,0 +1,6 @@
+import { PrismaClient } from '@prisma/client';
+
+// const prisma = new PrismaClient({ datasources: {  db: { url: "postgres://changelog_api_user:hODYHBaMJHPt56EJdnJfRUjYsksJYPOj@dpg-cdrai92en0hgm2vafkf0-a.frankfurt-postgres.render.com/changelog_api" } } });
+const prisma = new PrismaClient();
+
+export default prisma;
