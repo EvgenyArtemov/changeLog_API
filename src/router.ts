c49ea1f2a87946d22ea91504b782f1d208fe504e
Evgeny Artemov <eartemov013@gmail.com>
2022-11-20 22:38:32 +0400

    add validation middleware

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import { resolve } from 'path';
+import { body } from 'express-validator';
+import { handleInputErrors } from './modules/middleware';
 
 const router = Router();
 
@@ -10,22 +11,44 @@ router.get('/product', (req, res) => {
   res.end();
 });
 router.get('/product/:id', () => {});
-router.put('/product/:id', () => {});
-router.post('/product/', () => {});
+router.put('/product/:id', body('name').isString(), handleInputErrors, (req,res) => {
+  
+});
+router.post('/product/', body('name').isString(), () => {});
 router.delete('/product/:id', () => {});
 
 // UPDATE
 router.get('/update', () => {});
 router.get('/update/:id', () => {});
-router.put('/update/:id', () => {});
-router.post('/update/', () => {});
+router.put('/update/:id',
+  body('title').optional(),
+  body('body').optional(),
+  body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']).optional(),
+  body('version').optional(),
+  () => {}
+);
+router.post('/update/',
+  body('title').exists().isString(),
+  body('body').exists().isString(),
+  body('productId').exists().isString(),
+  () => {}
+);
 router.delete('/update/:id', () => {});
 
 // UPDATE POINT
 router.get('/updatepoint', () => {});
 router.get('/updatepoint/:id', () => {});
-router.put('/updatepoint/:id', () => {});
-router.post('/updatepoint/', () => {});
+router.put('/updatepoint/:id',
+  body('name').optional().isString(), 
+  body('description').optional().isString(),
+  () => {}
+);
+router.post('/updatepoint/',
+  body('name').isString(), 
+  body('description').isString(),
+  body('updateId').exists().isString(),
+  () => {}
+);
 router.delete('/updatepoint/:id', () => {});
 
 export default router
