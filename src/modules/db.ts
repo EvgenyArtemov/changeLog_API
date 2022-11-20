e2dbe0e02a130c8f0dc47ec482d7090a6c8bbf50
Evgeny Artemov <eartemov013@gmail.com>
2022-11-20 17:47:35 +0400

    fix error with wrong DB url

--- src/modules/db.ts
+++ src/modules/db.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({ datasources: {  db: { url: "postgres://changelog_api_user:hODYHBaMJHPt56EJdnJfRUjYsksJYPOj@dpg-cdrai92en0hgm2vafkf0-a.frankfurt-postgres.render.com/changelog_api" } } });
 
 export default prisma;
