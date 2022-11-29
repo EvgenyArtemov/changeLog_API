abec5cc7efd8fb6fffff7364934b1d4abc0d7d57
Evgeny Artemov <eartemov013@gmail.com>
2022-11-29 22:04:54 +0400

    add simple err handling

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import router from './router';
 import morgan from 'morgan';
 import { protect }from './modules/auth';
 import { createNewUser, signIn } from './handlers/user';
+import e from 'express';
 
 const app = express();
 
@@ -19,5 +20,15 @@ app.use('/api', protect, router);
 
 app.post('/user', createNewUser);
 app.post('/signin', signIn);
+
+app.use((err,req,res,next) => {
+  if(err.type === 'auth') {
+    res.status(401).json({ message: 'Unauthorized!' });
+  } else if(err.type === 'input') {
+    res.status(400).json({ message: 'Invalid input!' });
+  } else {
+    res.status(500).json({ message: 'Internal server error!' });
+  }
+})
 
 export default app;
